8aa8483129c473d6bb860a09611b0bba419f700a
Shikhar Mishra <mshikhar353@gmail.com>
2026-04-21 19:22:33 +0530

    Day1: Two Sum problem

---
+++ day1-two-sum.js
@@ -0,0 +1,13 @@
+// Day 1 - Two Sum
+// Platform: LeetCode
+// Topic: Array
+
+var twoSum = function(nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j];
+            }
+        }
+    }
+};
